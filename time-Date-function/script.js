42b7ef9ed35d30d7973b89ba445f1c0e06bd17ec
naimur <naimur202163@gmail.com>
2022-07-13 12:07:32 +0600

    javascript date function added

---
+++ time-Date-function/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello World")
+
+
+document.getElementById("button").addEventListener('click',()=>{
+    const date=Date()
+    console.log(date)
+document.getElementById("demo").innerHTML = date;
+
+        
+})
+
+
