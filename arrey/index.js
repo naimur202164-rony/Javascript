174d274a1a2c2a85e6974389c4ff259d3865f9bc
naimur <naimur202163@gmail.com>
2022-07-14 16:14:10 +0600

    day1

--- arrey/index.js
+++
@@ -1,8 +0,0 @@
-// console.log("Hello World");
-
-const numbers = [45, 4, 9, 16, 25];
-
-
-numbers.map(()=>{
-    console.log(numbers)
-});
--- currentWindow/script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello World");
-document.getElementById("button").addEventListener("click", () => {
-  window.print("hello WOlrd");
-});
-
-// let rony = "Helo";
