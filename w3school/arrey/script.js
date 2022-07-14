e9805b23b5bc11c9cd581774a965b7cd2e925aa5
naimur <naimur202163@gmail.com>
2022-07-14 20:01:15 +0600

    accesing last index in arrey methods

--- w3school/arrey/script.js
+++ w3school/arrey/script.js
@@ -10,3 +10,26 @@ console.log(cars[0]);
 console.log(cars[1]);
 console.log(cars[2]);
 // console.log(cars[3])
+
+// Objects Methods
+
+
+const person={
+    name:"Naimur Rahman",
+    age:23,
+    class:"Horns Frist Year Student",
+    height:"5.8 Feet"
+
+
+};
+const data=["Naimr","Rony","Mario","Niki"]
+
+
+console.log(person)
+
+// Array Properties and Methods
+console.log(data.length -1)
+// Accessing arrey last index
+const d=data[data.length-1];
+
+console.log(d)
