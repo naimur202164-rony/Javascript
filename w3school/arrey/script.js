80d259c721dfb46947f874dc8d8ccb3f2e44b31a
naimur <naimur202163@gmail.com>
2022-07-14 19:55:41 +0600

    arrey methods added

---
+++ w3school/arrey/script.js
@@ -0,0 +1,12 @@
+// console.log("HEllo From Arrey Methods")
+
+
+const cars=["Sabb","Volvo","BMW"];
+
+
+// Accessing on arrey
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+// console.log(cars[3])
--- w3school/stringSearch/index.js
+++ w3school/stringSearch/index.js
@@ -21,4 +21,19 @@ console.log( r.toPrecision())
 // The Value Of methods
 
 let f=123;
-console.log(typeof(f.valueOf()))
+console.log(typeof(f.valueOf()));
+
+
+// Numbers Methods
+Number(true);
+Number(false);
+Number("10");
+Number("  10");
+Number("10  ");
+Number(" 10  ");
+Number("10.33");
+Number("10,33");
+Number("10 33");
+Number("John");
+
+
