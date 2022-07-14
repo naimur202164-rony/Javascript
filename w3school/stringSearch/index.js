df6e0dc6c37cae349821b245542875c6194a82f0
naimur <naimur202163@gmail.com>
2022-07-14 19:45:50 +0600

    javascript value of methood added

--- w3school/stringSearch/index.js
+++ w3school/stringSearch/index.js
@@ -1,2 +1,24 @@
-console.log("Hello World")
-print("Hello WOrld")
+// console.log("Hello From Node")
+
+let x=123;
+console.log(typeof( x.toString()))
+
+const y=5.5;
+console.log(y.toExponential())
+
+
+// to Fixed 
+let r=7.4;
+console.log(r.toFixed())
+
+
+// to precision Methods
+
+let n=7.32;
+
+console.log( r.toPrecision())
+
+// The Value Of methods
+
+let f=123;
+console.log(typeof(f.valueOf()))
