cd2bf72ddd9086a1b75c7eb8dad6f66d78e54803
naimur <naimur202163@gmail.com>
2022-07-20 15:20:24 +0600

    javascript obj function added

---
+++ functional-programing/class4.js
@@ -0,0 +1,30 @@
+function name(fristName, lastName, gender) {
+  var output;
+  if (gender == "male") {
+    output = "MR" + fristName + "" + lastName;
+    return output;
+  } else if (gender == "female") {
+    var output = "MS  " + fristName + "" + lastName;
+    output;
+  }
+  return output;
+}
+
+var result = name("Naimur", "Rahman", "male");
+
+// console.log(result);
+
+function example() {
+  return {
+    name: "Naimur Rahman ",
+    skill: ["Java", "javascript"],
+    print: function () {
+      console.log(this.name, this.skill);
+    },
+  };
+}
+
+
+var obj=example();
+
+console.log(obj)
