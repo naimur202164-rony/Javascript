5495eb5778687cf28d278e903d8f105dfb7cb14a
naimur <naimur202163@gmail.com>
2022-07-21 10:40:25 +0600

    class7 added

---
+++ functional-programing/class6.js
@@ -0,0 +1,25 @@
+// Javascript Call Back Function
+
+// function hello(name) {
+//   print(name);
+//   print('SomeThing Else');
+// }
+
+// function print(args) {
+//   console.log(args);
+// }
+
+// console.log(hello("TWincle Cats"));
+
+let me = {
+  name: "This is Naimur Rahman",
+  age: 21,
+  email: "naimur202163@gmail.com",
+};
+
+function printMySelf(person, callback) {
+  console.log("person" + person);
+  if (age > 18) {
+    callback(email);
+  }
+}
