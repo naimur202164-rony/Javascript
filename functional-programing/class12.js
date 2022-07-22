8732f8c5f5aa6f14ced3efb74dcfef7cba6d8455
naimur <naimur202163@gmail.com>
2022-07-22 09:43:47 +0600

    javscript map function added

---
+++ functional-programing/class12.js
@@ -0,0 +1,20 @@
+var arr = [1, 2, 3, 4, 6, 2, 8, 9];
+
+// var newArr = [];
+
+// for (var i = 0; i < arr.length; i++) {
+
+//     newArr.push(arr[i]+2)
+// }
+
+
+
+let newArr=arr.map(()=>{
+    console.log(arr)
+})
+
+
+
+
+// console.log(newArr)
+// Class 12
