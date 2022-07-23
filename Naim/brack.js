5d792056af1263eaa16c72ae6dc6fa4988636982
naimur <naimur202163@gmail.com>
2022-07-23 20:54:35 +0600

    continue statement added

--- Naim/brack.js
+++ Naim/brack.js
@@ -4,6 +4,16 @@ for (var i = 1; i < 10; i++) {
   if (i % 5 == 0) {
     break;
   } else {
-    console.log(i);
+    // console.log(i);
+  }
+}
+
+// Continue Statement
+
+for (let i = 1; i < 10; i++) {
+  if (i === 3 || i === 7) {
+    continue;
+  }else{
+    console.log(i)
   }
 }
