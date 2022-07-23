ad552c6c8b38d10f6b722b71fb5c72df5137ca67
naimur <naimur202163@gmail.com>
2022-07-23 11:19:02 +0600

    javascript math methods are added

---
+++ Naim/math.js
@@ -0,0 +1,13 @@
+console.log(Math.E);
+console.log(Math.PI);
+
+console.log(3.121211);
+
+let n = 3.12164352321;
+console.log(Math.abs(n));
+
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+console.log(Math.max(400,500));
