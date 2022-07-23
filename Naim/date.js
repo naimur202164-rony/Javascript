3ce519bc24fee5c82fb43933575b1455396400b7
naimur <naimur202163@gmail.com>
2022-07-23 11:35:57 +0600

    javascript date mathods added

---
+++ Naim/date.js
@@ -0,0 +1,10 @@
+let date = new Date();
+console.log(date);
+console.log(date.toTimeString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getHours())
+console.log(date.getMilliseconds());
+console.log(date.getTime())
