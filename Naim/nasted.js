07ee9a80f92480147732813227a4bf083895f7ea
naimur <naimur202163@gmail.com>
2022-07-23 20:43:46 +0600

    javascript nasted loop added

--- Naim/nasted.js
+++ Naim/nasted.js
@@ -1,3 +1,10 @@
 var n = 5;
 
-for (var i = 1; i < name; i++) {}
+for (var i = 1; i <= n; i++) {
+  //   console.log(i);
+  let result = "";
+
+  for (var j = 1; j <= i; j++) {
+    console.log((result += j + ""));
+  }
+}
