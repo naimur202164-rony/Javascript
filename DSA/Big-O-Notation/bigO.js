fb582ecb131a53933b990b4a9adaca509fc7fc38
naimur <naimur202163@gmail.com>
2022-07-24 07:35:23 +0600

    javascript big o notation working

---
+++ DSA/Big-O-Notation/bigO.js
@@ -0,0 +1,18 @@
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i < n; i++) {
+    total += i;
+  }
+
+  return total;
+}
+
+// console.log(addUpTo(6));
+
+const addupto = (n) => {
+  return (n * (n + 1)) / 2;
+};
+
+
+
+console.log(addUpTo(10))
