311296bd0ab694d1cdca443066639219e8facd07
naimur <naimur202163@gmail.com>
2022-07-25 09:15:40 +0600

    arrey slice method are added

--- Naim/mide/arrey.js
+++ Naim/mide/arrey.js
@@ -7,6 +7,17 @@ const cars = ["rony", "Nahid", "Ridoy"];
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
+console.log((arr[3] = 9));
+console.log(
+// arr.slice(2, 0, 1, 29, 42, 1, 3421)
+
+);
+arr.push(8);
+arr.unshift(5);
+// console.log(arr);
+
+// Arrey Insert and remove
+
 for (let i = 0; i < arr.length; i++) {
-//   console.log((arr[i] = arr[i] + 2));
+  //   console.log((arr[i] = arr[i] + 2));
 }
