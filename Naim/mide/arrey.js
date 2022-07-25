9f47d2eeed5e0da180732523cfe2955b5cb97afc
naimur <naimur202163@gmail.com>
2022-07-25 09:01:49 +0600

    arrey travarse added

--- Naim/mide/arrey.js
+++ Naim/mide/arrey.js
@@ -1,6 +1,12 @@
 // 43 number video\
 
 const cars = ["rony", "Nahid", "Ridoy"];
-cars[1]="Fuker"
-console.log(cars[1]);
-console.log(cars.length)
+// cars[1]="Fuker"
+// console.log(cars[1]);
+// console.log(cars.length)
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for (let i = 0; i < arr.length; i++) {
+//   console.log((arr[i] = arr[i] + 2));
+}
