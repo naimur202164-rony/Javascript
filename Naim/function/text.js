2708eb84156c269ddacdd73cf86cc866959d69da
naimur <naimur202163@gmail.com>
2022-07-26 14:49:28 +0600

    function arrguments added

--- Naim/function/text.js
+++ Naim/function/text.js
@@ -1,3 +1,9 @@
-var date = new Date();
+// var date = new Date();
 
-console.log(date.getFullYear());
+// console.log(date.getFullYear());
+
+function test(a, b, c) {
+  console.log(a, b, c);
+}
+
+test(10, 2, 8);
