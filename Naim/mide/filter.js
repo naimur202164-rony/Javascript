4c7e091ebc3f7d36f8783db6c29135702119a7e0
naimur <naimur202163@gmail.com>
2022-08-01 19:25:05 +0600

    working on filter mahtod in javascript

--- Naim/mide/filter.js
+++ Naim/mide/filter.js
@@ -1,7 +1,19 @@
 const arr = [4, 8, 1, 3, 5, 6, 4, 3, 9];
 
-var filterArr = arr.filter(function (value) {
-  return value >4
-});
+// var filterArr = arr.filter(function (value) {
+//   return value >4
+// });
 
-console.log(filterArr);
+// console.log(filterArr);
+
+
+
+
+function myFilter(arr){
+    for(let i=0;i<arr.length;i++){
+        console.log(arr[i])
+    }
+}
+
+
+myFilter()
