64461768690e850c75a61f9508f8ea3e77c81912
naimur <naimur202163@gmail.com>
2022-08-01 14:56:05 +0600

    learning on javascript callback function

---
+++ Naim/mide/callback.js
@@ -0,0 +1,13 @@
+function sample(a, b) {
+  var c = a + b;
+  var d = a - b;
+  var result = sum(c, d);
+
+  return result
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sample(5,8))
--- Naim/mide/closer.js
+++ Naim/mide/closer.js
@@ -12,4 +12,4 @@ function a() {
 var abc=a();
 
 
-console.dir(abc)
+console.dir(abc);
