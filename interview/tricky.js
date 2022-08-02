e34f41699236cd31da5a1a5f9816bd29649086df
naimur <naimur202163@gmail.com>
2022-08-02 09:12:25 +0600

    string is added

--- interview/tricky.js
+++ interview/tricky.js
@@ -1,5 +1,5 @@
-const x = [1, 2, 3];
-x[-1] = -1;
+// const x = [1, 2, 3];
+// x[-1] = -1;
 
 // console.log(x[x.indexOf(10000)]);
 
@@ -15,3 +15,12 @@ let i=Number.MIN_VALUE;
 // console.log(i+1);//i=1;
 // console.log(i-1);//i=-1
 // console.log(i/i);//1
+
+
+let x=[1,2,3]+[4,5,6];
+
+
+console.log(x)
+
+
+
