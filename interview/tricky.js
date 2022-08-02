17163f9c38180585e0682eea7fbfaba5277e0ff9
naimur <naimur202163@gmail.com>
2022-08-02 09:02:59 +0600

    javascript min value is added

--- interview/tricky.js
+++ interview/tricky.js
@@ -1,17 +1,17 @@
-const x=[1,2,3];
-x[-1]=-1;
-
+const x = [1, 2, 3];
+x[-1] = -1;
 
 // console.log(x[x.indexOf(10000)]);
 
+const ary = [1, 2, 15, 30, 5, 45, 7];
 
-const ary=[1,2,15,30,5,45,7];
-
-
-
-console.log(
+// console.log(ary.sort());
 
 
-ary.sort()
+let i=Number.MIN_VALUE;
+// console.log(i)
 
-)
+// console.log(i*i); //i=o;;
+// console.log(i+1);//i=1;
+// console.log(i-1);//i=-1
+// console.log(i/i);//1
