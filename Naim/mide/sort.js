f33e946d8c4f59539029126b113dbd5cb6dcf9a0
naimur <naimur202163@gmail.com>
2022-08-03 19:18:18 +0600

    sort algo is added

--- Naim/mide/sort.js
+++ Naim/mide/sort.js
@@ -17,9 +17,19 @@ var perosn = [
   },
 ];
 
-var arr = [4, 8, 1, 6, 7, 9, 4, 3, 5, 6, 8, 2, 1, 7];
+var arr = [4, 8, 1, 6, 7, 9, 4, -7, 0, -2, 3, 5, 6, 8, 2, 1, 7, -4];
 
+arr.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
 
 arr.sort();
-
-console.log(arr)
+perosn.sort();
+console.log(perosn);
+console.log(arr);
