c70f889a0098405d3ddd2798c079036b04748da4
naimur <naimur202163@gmail.com>
2022-08-03 19:14:14 +0600

    arrey sort is added

---
+++ Naim/mide/sort.js
@@ -0,0 +1,25 @@
+var perosn = [
+  {
+    name: "A",
+    age: 24,
+  },
+  {
+    name: "B",
+    age: 19,
+  },
+  {
+    name: "C",
+    age: 26,
+  },
+  {
+    name: "D",
+    age: 21,
+  },
+];
+
+var arr = [4, 8, 1, 6, 7, 9, 4, 3, 5, 6, 8, 2, 1, 7];
+
+
+arr.sort();
+
+console.log(arr)
