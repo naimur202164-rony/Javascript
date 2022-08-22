061ea0e4574af1a1f6832c5fceaee25788220b0f
Your Name <naimur202163@gmail.com>
2022-08-22 19:57:08 +0600

    javascript string added

---
+++ jonas/funda1/12/script.js
@@ -0,0 +1,8 @@
+// Working with string
+const fristName="Naimur Rahman";
+let job="Student";
+let birthYear=2000;
+
+
+
+console.log(`my name is ${fristName} my job is ${job} and my birth day is ${birthYear}`);
