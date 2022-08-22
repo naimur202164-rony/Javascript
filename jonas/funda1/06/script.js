782a0eb8e696eccff835ce520a513dd050d3257d
Your Name <naimur202163@gmail.com>
2022-08-22 08:30:29 +0600

    working on javascript data types

---
+++ jonas/funda1/06/script.js
@@ -0,0 +1,27 @@
+console.log("Working on data types")
+// There 7 data types
+
+// 1 Number
+let age=21;
+// 2 String
+let text="Naimur";
+// 3 Boolen
+console.log(true)
+// 4 undefined
+
+// 5null means empty detais
+
+//6 Simbol
+
+// BigInt
+
+
+let javascriptIdFun=true;
+console.log(javascriptIdFun)
+
+
+let string='Naimur'
+console.log(string);
+
+
+
