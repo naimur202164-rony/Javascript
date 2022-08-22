586f1406c058a7448b8a9eda4f1e729cfd384a7b
Your Name <naimur202163@gmail.com>
2022-08-22 14:02:16 +0600

    working let

--- jonas/funda1/08/script.js
+++ jonas/funda1/08/script.js
@@ -1,6 +1,19 @@
 console.log("Working on let const var");
-
-
 console.log("Learning Let const Var")
 
+
+
 let name="a"
+
+
+let age=19;
+
+age=21;
+
+let birthYear=2000;
+
+const job="Programer"
+
+var fname="Ronyy";
+
+var laptop="Naimur Rahmnan"
