8ea0e6834e0314fabc8735715417f01f788f814a
Your Name <naimur202163@gmail.com>
2022-08-23 06:58:30 +0600

    learning javascript strict mode

--- jonas/fundamentals-2/02/script.js
+++ jonas/fundamentals-2/02/script.js
@@ -1,1 +1,45 @@
 // working strict mode
+'use strict';
+
+
+let hasDrivingLisense=false;
+const pastTest=true;
+
+
+if(pastTest){
+    console.log("He can get the users data")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hello World")
