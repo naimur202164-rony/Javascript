f9f8e08c106a0a66bcd503e8f366a5800a3832a5
Your Name <naimur202163@gmail.com>
2022-08-23 08:12:16 +0600

    function compeleted

---
+++ jonas/fundamentals-2/06/script.js
@@ -0,0 +1,9 @@
+function cutFruitPices(){
+    return  fruit * 4;
+}
+
+function fruitProcessor(apples,oranges){
+    const juice=`jucie with ${apples } apples and ${oranges} oranges` ;
+
+    return juice;
+}
