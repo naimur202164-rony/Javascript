3436faf9ec2b7b3cfc6cc7e5ee9791c81bc5b3c5
Your Name <naimur202163@gmail.com>
2022-08-23 07:07:47 +0600

    working with functions

---
+++ jonas/fundamentals-2/03/script.js
@@ -0,0 +1,14 @@
+console.log("Hello")
+
+function loger(){
+    console.log('My Name is Jonas')
+}
+
+loger()
+
+
+function fruitProccessor(apple,oranges){
+    console.log(`${apple} and ${oranges}`);
+}
+
+fruitProccessor("apple","oranges");
