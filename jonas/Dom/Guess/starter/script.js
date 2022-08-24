4655bc7bb3607081536be6c77bd5f73325f9ed30
Your Name <naimur202163@gmail.com>
2022-08-24 07:08:17 +0600

    evenetListenr added

--- jonas/Dom/Guess/starter/script.js
+++ jonas/Dom/Guess/starter/script.js
@@ -1,10 +1,16 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 'j';
+// document.querySelector('.message').textContent = 'Correct Number';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 12;
+// document.querySelector('.score').textContent = 'j';
 
-console.log(document.querySelector('.guess').value=12);
-console.log(document.querySelector('.guess').value=12);
+// console.log(document.querySelector('.guess').value=12);
+// console.log(document.querySelector('.guess').value=12);
+
+
+document.querySelector('.check').addEventListener('click',()=>{
+    // console.log("Naimur Rahman")
+    console.log(document.querySelector('.guess').value)
+})
