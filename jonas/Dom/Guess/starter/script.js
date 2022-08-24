aea35b2b82f7be6830f0f9ece0054d7f5c83cf84
Your Name <naimur202163@gmail.com>
2022-08-24 08:21:01 +0600

    project done

--- jonas/Dom/Guess/starter/script.js
+++ jonas/Dom/Guess/starter/script.js
@@ -12,7 +12,14 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+console.log(
+
+
+
+// document.querySelector('.number').textContent = secretNumber
+
+
+)
 // Displaying the number
 
 document.querySelector('.check').addEventListener('click', () => {
@@ -22,6 +29,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = 'No Number';
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+
+    // Changeing Color
+
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too heigh';
@@ -33,7 +45,6 @@ document.querySelector('.check').addEventListener('click', () => {
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low';
-      document.querySelector('.message').textContent = 'Too heigh';
       score--;
     } else {
       document.querySelector('.message').textContent = 'you have lost the Game';
