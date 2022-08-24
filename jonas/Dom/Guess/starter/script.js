73edcadaa9b1f031f2476d33f80715a4e539bec7
Your Name <naimur202163@gmail.com>
2022-08-24 06:57:10 +0600

    value added

--- jonas/Dom/Guess/starter/script.js
+++ jonas/Dom/Guess/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 'j';
+
+console.log(document.querySelector('.guess').value=12);
