16f15a5c8a06364e0af7f32fb2482cda91f5195a
Your Name <naimur202163@gmail.com>
2022-08-24 07:52:39 +0600

    score added

--- jonas/Dom/Guess/starter/script.js
+++ jonas/Dom/Guess/starter/script.js
@@ -11,7 +11,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
-let  score = 20;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 // Displaying the number
 
@@ -22,12 +22,21 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = 'No Number';
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
-
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too heigh';
-    score--;
-    document.querySelector('.score').textContent=score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too heigh';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent('You Lost the message');
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      document.querySelector('.message').textContent = 'Too heigh';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'you have lost the Game';
+    }
   }
 });
