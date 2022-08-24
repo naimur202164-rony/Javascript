c01b248e5f22575afc4301713c7549cfd5dc7e45
Your Name <naimur202163@gmail.com>
2022-08-24 07:43:03 +0600

    score content added

--- jonas/Dom/Guess/starter/script.js
+++ jonas/Dom/Guess/starter/script.js
@@ -9,9 +9,10 @@
 // console.log(document.querySelector('.guess').value=12);
 // console.log(document.querySelector('.guess').value=12);
 
-const secretNumber=Math.trunc(Math.random()*20)+1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
-document.querySelector('.number').textContent=secretNumber;
+let  score = 20;
+document.querySelector('.number').textContent = secretNumber;
 // Displaying the number
 
 document.querySelector('.check').addEventListener('click', () => {
@@ -19,11 +20,14 @@ document.querySelector('.check').addEventListener('click', () => {
   console.log(typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
-  }else if(guess==secretNumber){
-    document.querySelector('.message').textContent="Correct Number"
-  }else if(guess>secretNumber){
-    document.querySelector('.message').textContent="Too heigh"
-  }else if(guess <secretNumber){
-    document.querySelector('.message').textContent="Too Low"
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too heigh';
+    score--;
+    document.querySelector('.score').textContent=score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
   }
 });
