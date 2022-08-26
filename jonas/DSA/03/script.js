c541f75b276084561578904f646545e2a182b392
Your Name <naimur202163@gmail.com>
2022-08-26 08:26:16 +0600

    working with this

--- jonas/DSA/03/script.js
+++ jonas/DSA/03/script.js
@@ -1,35 +1,42 @@
 const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  
-    openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-  };
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
-  // Working with arrey dis
+  order:function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],[this.mainMenu[mainIndex]]];
+  }
+};
 
-  const arr=[2,3,4];
-
-const a=arr[0];
-const [x,y,z]=arr;
-console.log(x,y,z);
-
-
-let name="Naimur Rahman D";
-
+// Working with arrey discription
+const arr = [2, 3, 4];
+const a = arr[0];
+const [x, y, z] = arr;
+console.log(x, y, z);
+let name = "Naimur Rahman D";
 console.log(name);
+console.log("Naimur Rahman D");
+console.log(arr);
+
+const [frist,secound]=restaurant.categories;
+
+console.log(frist,secound);
+
+console.log(restaurant.order(2,8))
