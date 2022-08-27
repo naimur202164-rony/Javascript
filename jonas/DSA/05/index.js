9346fcdf04a3ad2f6d294f2c7a68e5749e6d0256
Your Name <naimur202163@gmail.com>
2022-08-27 08:20:08 +0600

    rest parameeters are added

--- jonas/DSA/05/index.js
+++ jonas/DSA/05/index.js
@@ -1,58 +1,51 @@
 const restaurant = {
-    name: "Classico Italiano",
-    location: "Via Angelo Tavanti 23, Firenze, Italy",
-    categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-    starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-    mainMenu: ["Pizza", "Pasta", "Risotto"],
-    openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-  
-    order: function (starterIndex, mainIndex) {
-      return [this.starterMenu[starterIndex], [this.mainMenu[mainIndex]]];
+    fri: {
+      open: 11,
+      close: 23,
     },
-  
-    orderDelivary: function ({ mainIndex, starterIndex, time, address }) {
-      console.log(
-        `order Recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deleverd to${address} at ${time} `
-      );
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
     },
-  };
-  
+  },
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], [this.mainMenu[mainIndex]]];
+  },
 
-  const arr=[7,8,9];
+  orderDelivary: function ({ mainIndex, starterIndex, time, address }) {
+    console.log(
+      `order Recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deleverd to${address} at ${time} `
+    );
+  },
+};
 
-  const newArr=[1,2,3,...arr];
+const arr = [7, 8, 9];
 
-  console.log(newArr);
+const newArr = [1, 2, 3, ...arr];
 
+console.log(newArr);
 
-  const newMenu=[...restaurant.mainMenu,'Gnocci'];
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
 
-  console.log(newMenu);
+console.log(newMenu);
 
+// Copy Arry;
 
-  // Copy Arry;
+const mainMenuCopy = [...restaurant.mainMenu];
 
+// Join to arrey
 
+const main = [...restaurant.mainMenu, ...restaurant.starterMenu];
 
-  const mainMenuCopy=[...restaurant.mainMenu];
-
-
-  // Join to arrey
-
-const main=[...restaurant.mainMenu,...restaurant.starterMenu];
-
-console.log(main)
+console.log(main);
