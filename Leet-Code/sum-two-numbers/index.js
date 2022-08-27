cc0e1ccc4367494717e84aafb27734119c099c62
Your Name <naimur202163@gmail.com>
2022-08-27 17:15:32 +0600

    trying to solve the problem

---
+++ Leet-Code/sum-two-numbers/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+ var twoSum = function(nums, target) {
+    let sum=0;
+    nums.forEach((arr,i)=>{
+        sum=arr;
+        // console.log(arr,i);
+        // console.log(i+i);
+        if(i+i==9){
+            console.log("h")
+        }
+        nums[0];
+        // console.log(nums[i]+nums[i]==9);
+        if(nums[i+i]==9){
+            console.log(ss)
+        }
+    
+    })
+    // console.log(sum)
+};
+
+
+twoSum([2,7,11,15],9)
