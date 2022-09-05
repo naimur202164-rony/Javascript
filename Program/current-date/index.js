b54a3dcb36bd440e5984e2a57fdecf5cc66f8b00
Your Name <naimur202163@gmail.com>
2022-09-05 09:13:42 +0600

    time added

--- Program/current-date/index.js
+++ Program/current-date/index.js
@@ -1,16 +1,18 @@
 const days = [
-    "monday",
-    "Tusday",
-    "Wed",
-    "thusday",
-    "firday",
-    "Satarday",
-    "Sunday",
-  ];
-  const d = new Date();
-const today = document.querySelector(".today").innerHTML=days[d.getDay()];
+  "monday",
+  "Tusday",
+  "Wed",
+  "thusday",
+  "firday",
+  "Satarday",
+  "Sunday",
+];
+const d = new Date();
+const today = (document.querySelector(".today").innerHTML = days[d.getDay()]);
+// const horus =
+// const mintes = d.getMinutes();
+const time = (document.querySelector(".time").innerHTML =  d.getHours()
+//   d.getHours()
 
-console.log();
-
-
-
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
