2dcf1a2e08d92a8e21d8e04a3fd8b86a33a08064
naimur <naimur202163@gmail.com>
2022-09-08 19:01:09 +0545

    javascript diffrence added

--- Program/difference/script.js
+++ Program/difference/script.js
@@ -1,1 +1,13 @@
 console.log("Naimiur Rahman D");
+
+let a = 10;
+let num1 = 20;
+if (num1 > 19) {
+  let sum = num1 - 19;
+  console.log(sum)
+  let mul = num1 * 3;
+  console.log(mul);
+} else {
+  sum = num1 - 19;
+  console.log(sum);
+}
