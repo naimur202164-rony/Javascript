023e3845b4e48d40eac539cb7be92ada2eccc455
naimur <naimur202163@gmail.com>
2022-09-08 19:36:19 +0545

    integer added

---
+++ Program/19/script.js
@@ -0,0 +1,13 @@
+console.log("Naimur Rahman D");
+
+// Checking is the number is good or bad
+
+const sum = (num1) => {
+  if (num1 > 20 || 200 || 400) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(sum(21));
