b03911bbcf163b62477dd38d5165917f692f8654
naimur <naimur202163@gmail.com>
2022-09-08 19:21:23 +0545

    conditon added

---
+++ Program/check/script.js
@@ -0,0 +1,11 @@
+console.log("Naimur Rahman");
+
+const sum = (num1, num2, num3) => {
+  if (num1 == 50 || num2 == 50 || num1 + num2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(sum(50,50));
