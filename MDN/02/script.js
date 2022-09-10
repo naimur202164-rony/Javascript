dda3d059fbb4b4d83dba9b7421c23c607cc650ca
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-10 20:19:36 +0545

    learning javascript commit

---
+++ MDN/02/script.js
@@ -0,0 +1,7 @@
+console.log("Naimur Rahman ");
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
