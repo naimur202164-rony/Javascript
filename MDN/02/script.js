feae2f84f486f301e35b325f5e678514e26c9407
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-11 19:10:05 +0545

    switch statment added

--- MDN/02/script.js
+++ MDN/02/script.js
@@ -8,7 +8,6 @@ if (iceCream === "chocolate") {
 
 function multiplay(num1, num2) {
   let result = num1 * num2;
-
   return result;
 }
 
@@ -20,3 +19,19 @@ console.log(multiplay(10, 20));
 document.querySelector('html').addEventListener('click',()=>{
   alert("Dont toch me")
 })
+
+
+switch (expression) {
+  case choice1:
+    run this code
+    break;
+
+  case choice2:
+    run this code instead
+    break;
+
+  // include as many cases as you like
+
+  default:
+    actually, just run this code
+}
