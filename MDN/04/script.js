9b3a2cd5fd8661dd2b092034e3c4dd90432aa361
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-12 08:13:55 +0545

    javascript alert function is added

--- MDN/04/script.js
+++ MDN/04/script.js
@@ -1,3 +1,9 @@
 function random(number) {
-    return Math.floor(Math.random()*number);
-  }
+  return Math.floor(Math.random() * number);
+}
+
+function alerst() {
+  alert("This Is Naimur Rahman D");
+}
+
+alerst();
