54bc690433bf50fa57d1486bf35556c07ceed3fd
naimur <naimur202163@gmail.com>
2022-09-14 20:33:36 +0600

    nasted function added

--- MDN/05/script.js
+++ MDN/05/script.js
@@ -7,6 +7,15 @@ const person={
     order:"This is Naimur Rahman D",
     bio:function(){
         console.log(`${this.name[0]}  ${this.name[1]} ${this.age} is years old`)
-    }
+    },
+    introduceSelf:function(){
+        console.log(`Hi Iam ${this.name[0]}`);
+    },
 };
-console.log(person);
+console.log(person.name,
+
+        person.name[0],
+        person.age,
+      
+    
+    );
