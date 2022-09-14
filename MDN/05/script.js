24e1f10d6972aba1fc8d72805170f98678c01d14
naimur <naimur202163@gmail.com>
2022-09-14 20:30:04 +0600

    working objects

--- MDN/05/script.js
+++ MDN/05/script.js
@@ -1,1 +1,12 @@
-console.log("MDN")
+console.log("MDN");
+
+
+const person={
+    name:['Naimur','Smith'],
+    age:22,
+    order:"This is Naimur Rahman D",
+    bio:function(){
+        console.log(`${this.name[0]}  ${this.name[1]} ${this.age} is years old`)
+    }
+};
+console.log(person);
