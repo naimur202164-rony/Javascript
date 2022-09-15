62aad2d39d03d104139b3761a4f171aeda5a3249
naimur <naimur202163@gmail.com>
2022-09-15 19:16:49 +0600

    done

--- books/eloquentjavascript/lesson2/script.js
+++ books/eloquentjavascript/lesson2/script.js
@@ -2,4 +2,20 @@ let name="Naimur Rahman D"
 
 
 let sums=(100+4)*11;
-console.log(sums)
+// console.log(sums);
+
+// template literles
+
+
+let half=`half of 100 ${100/2}`;
+
+// console.log(half);
+
+// UNARY OPERATOR 
+// console.log(-(10-2));
+
+console.log(8*null);
+console.log('5'-1);
+console.log("5"+1);
+console.log("five"*2);
+console.log(false==0);
