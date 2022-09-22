c16ff7a52f968890a3b7ce55b842f2c79da9837c
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-22 12:39:09 +0545

    noise function added

--- books/eloquentjavascript/lession3/script.js
+++ books/eloquentjavascript/lession3/script.js
@@ -1,9 +1,16 @@
 // Working with function example
 
-const square=(x)=>{
-    return x*x
+const square = (x) => {
+  return x * x;
 };
 
-
-
 console.log(10);
+
+// Make Noise Function
+
+const makeNoise = () => {
+  console.log("Pling");
+};
+makeNoise();
+
+// 
