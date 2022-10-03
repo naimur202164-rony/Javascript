b77510f74c80a75b988177c83de6a22fff63cef8
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-03 14:06:36 +0545

    meal function added

--- MDN/08/script.js
+++ MDN/08/script.js
@@ -8,8 +8,24 @@ let meal = 12.0;
 let tip = 20;
 let tax = 8;
 
+function main() {
+    const meal_cost = parseFloat(12.0);
+
+    const tip_percent = parseInt(20)
+
+    const tax_percent = parseInt(8);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
+
 // OOP and JavaScript
 
 console.log("Hello");
 
 console.log("Ups");
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+    
+
+}
