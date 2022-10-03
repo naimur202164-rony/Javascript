b07fe1ec3df7518962f47d5a1b3242738473fbaa
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-03 08:18:45 +0545

    meal tip added

--- MDN/08/script.js
+++ MDN/08/script.js
@@ -1,18 +1,15 @@
 console.log("OPP");
 
-walsh=new Professor("Walsh,English");
+walsh = new Professor("Walsh,English");
 
 // walsh.in
 
-
-
-
+let meal = 12.0;
+let tip = 20;
+let tax = 8;
 
 // OOP and JavaScript
 
-
 console.log("Hello");
 
-console.log(
-    "Ups"
-)
+console.log("Ups");
