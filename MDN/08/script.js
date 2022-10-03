09e5bccc1fb1b58566fd492d769eed25b68de640
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-03 16:23:55 +0545

    food parseInt added

--- MDN/08/script.js
+++ MDN/08/script.js
@@ -7,25 +7,3 @@ walsh = new Professor("Walsh,English");
 let meal = 12.0;
 let tip = 20;
 let tax = 8;
-
-function main() {
-    const meal_cost = parseFloat(12.0);
-
-    const tip_percent = parseInt(20)
-
-    const tax_percent = parseInt(8);
-
-    solve(meal_cost, tip_percent, tax_percent);
-}
-
-// OOP and JavaScript
-
-console.log("Hello");
-
-console.log("Ups");
-
-function solve(meal_cost, tip_percent, tax_percent) {
-    // Write your code here
-    
-
-}
