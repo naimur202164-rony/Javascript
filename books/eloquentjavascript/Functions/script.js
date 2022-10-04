66d2e5bbc534d5751e79c5d50e6580d84c812974
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-05 03:09:31 +0545

    javascript power function added

--- books/eloquentjavascript/Functions/script.js
+++ books/eloquentjavascript/Functions/script.js
@@ -1,3 +1,29 @@
 // console.log("Naimur Rahman")
 
 // learning Function
+const square = function (x) {
+  return x * x;
+};
+
+console.log(square(10));
+// Calling Function
+
+const makeNoise = () => {
+  console.log("Ping");
+};
+
+makeNoise();
+
+
+
+
+const power = function(base, exponent) {
+    let result = 0;
+    for (let count = 0; count < exponent; count++) {
+      result *= bas1e;
+    }
+    return result;
+  };
+  
+  console.log(power(2, 10));
+  // → 1024
