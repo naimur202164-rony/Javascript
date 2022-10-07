98dd206894f7ccd923409b5ef1b297306b3ff9cc
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-07 20:45:38 +0545

    learning funcitions

--- books/eloquentjavascript/Functions/script.js
+++ books/eloquentjavascript/Functions/script.js
@@ -1,29 +1,41 @@
-// console.log("Naimur Rahman")
+// // console.log("Naimur Rahman")
 
-// learning Function
-const square = function (x) {
-  return x * x;
-};
+// // learning Function
+// const square = function (x) {
+//   return x * x;
+// };
 
-console.log(square(10));
-// Calling Function
+// console.log(square(10));
+// // Calling Function
 
-const makeNoise = () => {
-  console.log("Ping");
-};
+// const makeNoise = () => {
+//   console.log("Ping");
+// };
 
-makeNoise();
-
-
+// makeNoise();
 
 
-const power = function(base, exponent) {
-    let result = 0;
-    for (let count = 0; count < exponent; count++) {
-      result *= bas1e;
-    }
-    return result;
-  };
+
+
+// const power = function(base, exponent) {
+//     let result = 0;
+//     for (let count = 0; count < exponent; count++) {
+//       result *= bas1e;
+//     }
+//     return result;
+//   };
   
-  console.log(power(2, 10));
-  // → 1024
+//   console.log(power(2, 10));
+//   // → 1024
+
+
+// let x=10;
+// if(true){
+//   let y=20;
+//   let z
+// }
+
+
+let lanuchMissile=function(){
+  
+}
