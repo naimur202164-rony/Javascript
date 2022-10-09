6d448d31a58052b3433282bf2c4d86d7302030cd
naimur <naimur202163@gmail.com>
2022-10-09 20:48:43 +0600

    new function added

--- books/eloquentjavascript/Functions/script.js
+++ books/eloquentjavascript/Functions/script.js
@@ -49,3 +49,26 @@ function minus(a, b) {
   else return a - b;
 }
 console.log(minus(10, 5));
+
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+             find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
+// → (((1 * 3) + 5) * 3)
+
+
+
+
+
