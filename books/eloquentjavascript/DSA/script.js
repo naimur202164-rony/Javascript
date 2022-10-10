dd82e41d119ed4ff69816eca9c85b2528a51a33c
naimur <naimur202163@gmail.com>
2022-10-10 12:42:23 +0600

    working with arrey of objects

--- books/eloquentjavascript/DSA/script.js
+++ books/eloquentjavascript/DSA/script.js
@@ -1,1 +1,27 @@
 console.log("DSA IS WORKING ");
+let listOfNumbers = [2, 3, 5, 7, 11];
+
+console.log(listOfNumbers[4]);
+
+let journal = [
+  {
+    events: ["work", "touched tree", "pizza", "running", "television"],
+    squirrel: false,
+  },
+  {
+    events: [
+      "work",
+      "ice cream",
+      "cauliflower",
+      "lasagna",
+      "touched tree",
+      "brushed teeth",
+    ],
+    squirrel: false,
+  },
+  {
+    events: ["weekend", "cycling", "break", "peanuts", "beer"],
+    squirrel: true,
+  },
+  /* and so on... */
+];
