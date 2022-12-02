e3cb95d753becd09d9c546ce4485b6e77d8eee2b
naimur <naimur202163@gmail.com>
2022-12-02 18:57:56 +0600

    working with leet code problems

--- books/loop.js
+++ books/loop.js
@@ -1,12 +1,14 @@
-let total=0;count=1;
+let total = 0;
+count = 1;
 
-while(count<=10){
-    total +=count;
-    count +=1;
+while (count <= 10) {
+  total += count;
+  count += 1;
 }
-
 
 console.log(total);
 
-console.log(8*null);
-console.log("5"-1);
+console.log(8 * null);
+console.log("5" - 1);
+
+console.log("Naimur Rahman D");
