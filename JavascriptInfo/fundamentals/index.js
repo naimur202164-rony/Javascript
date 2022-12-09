1f2e896e1010a74a44ba9c49ce87bf0889f28035
naimur <naimur202163@gmail.com>
2022-12-09 21:36:37 +0600

    javascript code added

---
+++ JavascriptInfo/fundamentals/index.js
@@ -0,0 +1,14 @@
+let x = 12;
+
+switch (x) {
+  case 12:
+    console.log("same Number");
+    break;
+  case 8:
+    console.log("The number is low");
+    break;
+
+  case 13:
+    console.log("The Number is large");
+    break;
+}
